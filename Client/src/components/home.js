5b11b2473925af1f53e99b815bd48f564bbcb9ea
Maxim Shvartzbaum <maxim8844@gmail.com>
2022-03-21 15:27:03 +0200

    changed

--- Client/src/components/home.js
+++ Client/src/components/home.js
@@ -43,10 +43,10 @@ export default class Home extends Component {
 
     onChangePublisher(e) {
         console.log(e.target.value)
-        let publishers = e.target.value.split(",")
-        let tp = publishers.foreach(p => p.trim())
+        let publishers = publishers.trim(",")
+        //let tp = publishers.foreach(p => p.trim())
         this.setState({
-            publishers: tp
+            publishers
         });
         console.log(this.state.publishers)
       }
@@ -104,12 +104,12 @@ export default class Home extends Component {
                     <div>
                         <input type="button" value="publish new post" onClick={this.toggleNewPost.bind(this)} />
                         <br/>
-                        Date from: <input type="date" onChange={e => this.onChange(e.target.value.startDate)} />
-                        Date to: <input type="date" onChange={e => this.onChange(e.target.value.endDate)} />
+                        Date from: <input type="date" onChange={this.onChangeStartDate.bind(this)} />
+                        Date to: <input type="date" onChange={this.onChangeEndDate.bind(this)} />
                         Publishers: <input type="text" onChange={this.onChangePublisher.bind(this)} />
-                        Radius from current location: <input type="number" onChange={e => this.onChange(e.target.value.radius)} />
-                        Image tags: <input type="text" onChange={e => this.onChange(e.target.value.imageTags)} />
-                        Tagged users: <input type="text" onChange={e => this.onChange(e.target.value.taggedUsers)} />
+                        Radius from current location: <input type="number" onChange={this.onChangeRadius.bind(this)} />
+                        Image tags: <input type="text" onChange={this.onChangeImageTags.bind(this)} />
+                        Tagged users: <input type="text" onChange={this.onChangeTaggedUsers.bind(this)} />
                     </div>
                     <div><MapContainer searchInfo={this.state.publishers} currentLocation={this.handleCurrentLocation.bind(this)}/></div>
                 </div>
